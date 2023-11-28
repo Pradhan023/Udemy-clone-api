b46f1d81420dba0514c737d8ec243a6e2760a72c
Anish Pradhan <anishpradha5523@gmail.com>
2023-11-28 16:02:45 +0530

    Udemy-Clone-Api

---
+++ Data.js
@@ -0,0 +1,24 @@
+const AllData = [
+    {
+        id:1,
+        img:"https://img-c.udemycdn.com/course/240x135/8324_fa84_13.jpg",
+        heading:"Powerful Business Writing: How to Write Concisely",           
+        author:"Caroline McDevitt",
+        des:"A concise business writing course for punchy, professional and powerful writing – at work, at university, on your blog",
+        rating:"4.7",
+        price:499,
+        category:"Business",
+        subcat:"Communication"
+    },
+    {
+        id:,
+        img:"",
+        heading:"",           
+        author:"",
+        des:"",
+        rating:"4.7",
+        price:,
+        category:"Business",
+        subcat:"Communication"
+    },
+]
